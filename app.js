e55e29c802418adabd7b87dae90c71a67460b765
paddo83 <paddymcginley@icloud.com>
2024-05-09 21:27:47 +0100

    git innit

--- app.js
+++ app.js
@@ -27,3 +27,29 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+
+
+   var swiper = new swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+});
+
